14f1a52ff6c9f7a0ec193ef4679630a69888f188
jcamachomzamaxpro <jcamachomzamaxpro@gmail.com>
2023-09-18 22:28:21 -0500

    feat: :tada: actividad sept18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  // const handleDeleteTask = (index) => {
+  //   const updatedTasks = tasks.filter((_, i) => i !== index);
+  //   setTasks(updatedTasks);
+  // };
+
+  const handleDeleteTask = (index) => {
+    // Copiamos el array de tareas
+    const updatedTasks = [...tasks];
+
+    // Eliminamos la tarea en el índice especificado
+    updatedTasks.splice(index, 1);
+
+    // Actualizamos el estado con la nueva lista de tareas
+    setTasks(updatedTasks);
+  };
+
+  const handleEditTask = (index) => {
+    const editedTask = prompt("Edit task:", tasks[index]);
+    if (editedTask !== null) {
+      const updatedTasks = [...tasks];
+      updatedTasks[index] = editedTask;
+      setTasks(updatedTasks);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleEditTask(index)}>Edit</button>
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
